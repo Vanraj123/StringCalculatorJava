45d2f4261d1f8720a002deb7fc9f7a421c52ce57
vanraj <jayantiparmar8563@gmail.com>
2025-07-04 21:24:24 +0530

    Commit for Testcase number 2.

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,13 +1,13 @@
 public class StringCalculator {
     public int add(String numbers) {
-        if (numbers == null || numbers.isEmpty()) {
+        if (numbers == null || numbers.isEmpty()) { // for the empty or NULL string 
             return 0;
         }
 
-        String[] tokens = numbers.split(",");
+        String[] tokens = numbers.split(","); // seperate the number with comma
         int sum = 0;
 
-        for (String token : tokens) {
+        for (String token : tokens) {               // sum of number 
             sum += Integer.parseInt(token.trim());
         }
 
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -5,17 +5,17 @@ class StringCalculatorTest {
 
     StringCalculator calc = new StringCalculator();
 
-    @Test
+    @Test                                                           // For the empty string                                                  
     void testEmptyStringReturnsZero() {
         assertEquals(0, calc.add(""));
     }
 
-    @Test
+    @Test                                                           // For Null input As string
     void testNullStringReturnsZero() {
         assertEquals(0, calc.add(null));
     }
 
-    @Test
+    @Test                                                           // For the simple Number String 
     void testSingleNumberReturnsItself() {
         assertEquals(1, calc.add("1"));
         assertEquals(42, calc.add("42"));
@@ -37,4 +37,10 @@ class StringCalculatorTest {
     void testNumbersWithSpaces() {
         assertEquals(6, calc.add(" 1 , 2 , 3 "));
     }
+
+    @Test                                                           // For Any Amount of numbers
+    void testAnyAmountOfNumbers() {
+    assertEquals(510, calc.add("1,2,3,4,500"));
+    assertEquals(10045, calc.add("1,2,3,4,5,6,7,8,9,10000"));
+}
 }
