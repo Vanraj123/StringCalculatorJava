9d300f877ebddb1e33642a7abfb3e6a524f2fec1
vanraj <jayantiparmar8563@gmail.com>
2025-07-05 18:04:31 +0530

    Add Screenshots for Testcase3 example

--- src/main/java/StringCalculatorApp.java
+++ src/main/java/StringCalculatorApp.java
@@ -2,19 +2,23 @@ import java.util.Scanner;
 
 public class StringCalculatorApp {
     public static void main(String[] args) {
-        StringCalculator calculator = new StringCalculator();
+    StringCalculator calculator = new StringCalculator();
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter numbers string:");
-        String input = scanner.nextLine();
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter numbers string:");
+    String input = scanner.nextLine();
 
-        try {
-            int result = calculator.add(input);
-            System.out.println("Sum: " + result);
-        } catch (IllegalArgumentException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
+    // replace literal \n (from typing) with actual newline
+    input = input.replace("\\n", "\n");
 
-        scanner.close();
+    try {
+        int result = calculator.add(input);
+        System.out.println("Sum: " + result);
+    } catch (IllegalArgumentException e) {
+        System.out.println("Error: " + e.getMessage());
     }
+
+    scanner.close();
+}
+
 }
